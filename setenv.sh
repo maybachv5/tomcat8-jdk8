31cbb2e35823de3341d4e6e5012da8513194cc58
maybach <289727461@qq.com>
2018-04-28 18:05:56 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ATALINA_OPTS="-Xms1024m -Xmx4096m -Xss1024K -XX:PermSize=512m -XX:MaxPermSize=2048m -Duser.timezone=Asia/Shanghai -Dspring.profiles.active=${ENV}"
+CATALINA_OPTS="-Xms1024m -Xmx4096m -Xss1024K -XX:PermSize=512m -XX:MaxPermSize=2048m -Duser.timezone=Asia/Shanghai -Dspring.profiles.active=${ENV}"
